552247256158fab8fb007f476cec657cbece8bd6
Aayushi-Agrwal <khushiagarwal01464@gmail.com>
2023-10-19 14:10:51 +0530

    feat: add delete and edit functionality

--- todo-mysql/src/App.jsx
+++ todo-mysql/src/App.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import ToDoItem from "./ToDoItem";
 import "./App.css";
+import { ReactComponent as EditIcon } from "./pen-solid.svg";
+import { ReactComponent as DeleteIcon } from "./trash-solid.svg";
 
 function App() {
   const [inputText, setInputText] = useState("");
   const [items, setItems] = useState([]);
+  const [openEdit, setOpenEdit] = useState(-1);
+  const [editable, setEditable] = useState(-1);
 
   function handleChange(event) {
     const newValue = event.target.value;
@@ -24,10 +28,21 @@ function App() {
         return index !== id;
       });
     });
+    setOpenEdit(-1);
+  }
+
+  function editItem(id) {
+    setEditable(id);
+  }
+
+  function saveEdit(id, newValue) {
+    const newItems = [...items];
+    newItems[id] = newValue;
+    setItems(newItems);
   }
 
   return (
-    <div className="container">
+    <div className="container" onClick={() => setOpenEdit(-1)}>
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
@@ -40,12 +55,68 @@ function App() {
       <div>
         <ul>
           {items.map((todoItem, index) => (
-            <ToDoItem
-              key={index}
-              id={index}
-              text={todoItem}
-              onChecked={deleteItem}
-            />
+            <div style={{ padding: "1rem" }}>
+              {openEdit == index && (
+                <div className="editBox" onClick={(e) => e.stopPropagation()}>
+                  <ul className="editBoxList">
+                    <li
+                      className="editBoxListItem"
+                      onClick={() => editItem(index)}
+                    >
+                      <EditIcon
+                        style={{
+                          height: "1rem",
+                        }}
+                      />
+                      Edit
+                    </li>
+
+                    <li
+                      className="editBoxListItem"
+                      onClick={() => deleteItem(index)}
+                    >
+                      <DeleteIcon
+                        style={{
+                          height: "1rem",
+                        }}
+                      />
+                      Delete
+                    </li>
+                  </ul>
+                </div>
+              )}
+              {editable === index ? (
+                <div style={{ display: "flex" }}>
+                  <input
+                    type="text"
+                    value={todoItem}
+                    onChange={(e) => {
+                      saveEdit(index, e.target.value);
+                    }}
+                  />
+                  <li
+                    className="editBoxListItem"
+                    onClick={() => setEditable(-1)}
+                  >
+                    {/* <EditIcon
+                        style={{
+                          height: "1rem",
+                        }}
+                      /> */}
+                    Save
+                  </li>
+                </div>
+              ) : (
+                <ToDoItem
+                  key={index}
+                  id={index}
+                  text={todoItem}
+                  // onChecked={deleteItem}
+                  openEdit={openEdit}
+                  setOpenEdit={setOpenEdit}
+                />
+              )}
+            </div>
           ))}
         </ul>
       </div>
--- todo-mysql/src/ToDoItem.jsx
+++ todo-mysql/src/ToDoItem.jsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { ReactComponent as DetailsIcon } from "./ellipsis-vertical-solid.svg";
 
-function ToDoItem(props) {
+function ToDoItem({ id, text, openEdit, setOpenEdit, onChecked }) {
+  const [checked, setChecked] = useState(false);
   return (
-    <div
-      onClick={() => {
-        props.onChecked(props.id);
-      }}
-    >
-      <li>{props.text}</li>
+    <div>
+      <li>
+        <div
+          onClick={() => setChecked(!checked)}
+          className={checked && "checked"}
+        >
+          {text}
+        </div>
+        <div
+          onClick={(e) => {
+            openEdit === id ? setOpenEdit(-1) : setOpenEdit(id);
+            e.stopPropagation();
+          }}
+          style={{ padding: "0.5rem" }}
+        >
+          <DetailsIcon
+            style={{
+              height: "1.2rem",
+            }}
+          />
+        </div>
+      </li>
     </div>
   );
 }
